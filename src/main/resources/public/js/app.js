8beec8f0394d45ff3d98543f5ec86a45e19580ef
Lorenah <lorayne911@gmail.com>
2020-12-08 15:59:39 +0300

    added images and css styling

---
+++ src/main/resources/public/js/app.js
@@ -0,0 +1,44 @@
+// $(document).ready(function(){
+// // Animations
+//     new WOW().init();
+//
+//     let typed = new Typed(`#type`, {
+//         strings: [" Wild_Map  "],
+//         backSpeed: 70,
+//         typeSpeed: 80,
+//         smartBackspace: true,
+//         loop: true,
+//     });
+//
+//
+// function secondClick(){
+//     timer:4000
+//     Swal.fire(
+//         'View Animals?',
+//     )
+// }
+//
+// function firstClick(){
+//     timer:4000
+//     Swal.fire(
+//         'Add Endangered?',
+//
+//     )
+// }
+// function thirdClick(){
+//     timer:4000
+//     Swal.fire(
+//         'Add Sighting?',
+//
+//     )
+// }
+//
+//
+//
+//
+//
+//
+//
+// // Animations
+// new WOW().init();
+// }
