c63912dcfeba48c68178fc51d077c962c3b88f42
Lorenah <lorayne911@gmail.com>
2020-12-08 15:44:04 +0300

    ran tests

---
+++ src/test/java/AnimalTest.java
@@ -0,0 +1,22 @@
+import junit.framework.TestCase;
+
+public class AnimalTest extends TestCase {
+
+    public void testTestSetName() {
+    }
+
+    public void testTestEquals() {
+    }
+
+    public void testSave() {
+    }
+
+    public void testDelete() {
+    }
+
+    public void testAll() {
+    }
+
+    public void testFind() {
+    }
+}
