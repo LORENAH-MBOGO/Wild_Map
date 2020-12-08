8f204f2a792f59fc87b888dd9696d4f26a0c286b
Lorenah <lorayne911@gmail.com>
2020-12-08 16:38:50 +0300

    ran sighting tests

---
+++ src/test/java/SightingTest.java
@@ -0,0 +1,25 @@
+import junit.framework.TestCase;
+
+public class SightingTest extends TestCase {
+
+    public void testGetId() {
+    }
+
+    public void testGetAnimalId() {
+    }
+
+    public void testGetLocation() {
+    }
+
+    public void testGetRangerName() {
+    }
+
+    public void testTestEquals() {
+    }
+
+    public void testFind() {
+    }
+
+    public void testDelete() {
+    }
+}
